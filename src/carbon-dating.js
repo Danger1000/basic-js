465c85a82a7d7289cbdbb2631e15c2c8217d8424
Danger1000 <60090781+Danger1000@users.noreply.github.com>
2020-03-09 00:48:48 +0300

    feat: add carbon_dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,18 @@
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
-
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
+const period = 0.693 / HALF_LIFE_PERIOD;
+module.exports = function dateSample(sampleActiv) {
+  if (typeof sampleActiv != 'string' || hasLetters(sampleActiv) || invalidRange(sampleActiv)) {
+    return false;
+  }
+  const actualActivity = parseFloat(sampleActiv);
+  const age = Math.log(MODERN_ACTIVITY / actualActivity) / period;
+  return Math.ceil(age);
+};
+function hasLetters(str) {
+  return /[A-Za-z]/.test(str)
+}
+function invalidRange(activ) {
+  const float = parseFloat(activ);
+  return float <= 0 || float >= MODERN_ACTIVITY;
 };
