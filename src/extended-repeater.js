16e278fb8eaa4d72e7f7c7ea06d62b2cf998d93b
Danger1000 <60090781+Danger1000@users.noreply.github.com>
2020-03-10 20:50:57 +0300

    feat: add extended-repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,18 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  options = Object.assign({
+  repeatTimes: 0,
+  separator: '+',
+  addition: '',
+  additionRepeatTimes: 0,
+  additionSeparator: ''
+  }, options);
+  let arrAdditional = [];
+  for (let j = 0; j < (options.additionRepeatTimes ? options.additionRepeatTimes : 1); j++) {
+    arrAdditional.push(options.addition + '');
+  }
+  let arrResult = [];
+  for (let i = 0; i < (options.repeatTimes ? options.repeatTimes : 1); i++) {
+    arrResult.push(str + arrAdditional.join(options.additionSeparator));
+  }
+  return arrResult.join(options.separator);
 };
-  
